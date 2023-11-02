fc5996cfc6f8f6d91873818327b2bec0cc6022ec
Jad Jaber <jabe0043@algonquinlive.com>
2023-11-02 18:49:53 -0400

    Move fetchUsers and output method to separate class called FetchClass

--- bin/dart_hybrid_4.dart
+++ bin/dart_hybrid_4.dart
@@ -1,31 +1,8 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
+import 'package:dart_hybrid_4/dart_hybrid_4.dart';
 
 void main() async {
-  try {
-    var randomUsers = await fetchUsers(10);
-    if (randomUsers != null) {
-      for (var user in randomUsers) {
-        var firstName = user['first_name'];
-        var lastName = user['last_name'];
-        var uid = user['uid'];
-        print('First Name: $firstName | Last Name: $lastName | UID: $uid');
-      }
-    }
-  } catch (error) {
-    print('ERROR: $error');
-  }
-}
+  var fetchClass = FetchClass();
 
-// fetch function
-Future<List<Map<String, dynamic>>> fetchUsers(int num) async {
-  var url = Uri.parse('https://random-data-api.com/api/v2/users?size=$num');
-
-  var res = await http.get(url);
-  if (res.statusCode == 200) {
-    final List<dynamic> data = convert.jsonDecode(res.body);
-    return data.cast<Map<String, dynamic>>();
-  } else {
-    throw Exception('Fetch Error');
-  }
+  var randomUsers = await fetchClass.fetchUsers(10); //fetchUsers
+  fetchClass.printUsers(randomUsers); //Output users
 }
--- lib/dart_hybrid_4.dart
+++ lib/dart_hybrid_4.dart
@@ -1,1 +1,31 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
+class FetchClass {
+// Fetch function
+  Future<List<Map<String, dynamic>>> fetchUsers(int num) async {
+    var url = Uri.parse('https://random-data-api.com/api/v2/users?size=$num');
+
+    try {
+      var res = await http.get(url);
+      if (res.statusCode == 200) {
+        final List<dynamic> data = convert.jsonDecode(res.body);
+        return data.cast<Map<String, dynamic>>();
+      } else {
+        throw Exception('Fetch Error');
+      }
+    } catch (error) {
+      throw Exception('ERROR: $error');
+    }
+  }
+
+// Output function
+  void printUsers(List<Map<String, dynamic>> users) {
+    for (var user in users) {
+      var firstName = user['first_name'];
+      var lastName = user['last_name'];
+      var uid = user['uid'];
+      print('First Name: $firstName | Last Name: $lastName | UID: $uid');
+    }
+  }
+}
